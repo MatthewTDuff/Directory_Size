2e7fcbb393c079accbeeada239ed866c942a8b71
Matthew <duffm98@gmail.com>
2018-10-29 19:33:44 -0500

    Discovered my system was running in python 2, not 3. Reverted code.

--- directory_size.py
+++ directory_size.py
@@ -1,26 +1,25 @@
 
 
 import os
-def get_size(start_path='.'):
-    total_size = 0
-    seen = {}
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            try:
-                stat = os.stat(fp)
-            except OSError:
-                continue
 
-            try:
-                seen[stat.st_ino]
-            except KeyError:
-                seen[stat.st_ino] = True
-            else:
-                continue
+def getFolderSize(folder):
+    for item in os.scandir(folder):
+        if item.is_file():
+            print("FILE");
+        elif item.is_dir():
+            getFolderSize(item.path)
+            print("DIR")
 
-            total_size += stat.st_size
+"""
+def folder_size(path='.'):
+    total = 0
+    for entry in os.scandir(path):
+        if entry.is_file():
+            total += entry.stat().st_size
+        elif entry.is_dir():
+            total += folder_size(entry.path)
+    return total
+"""
 
-    return total_size
 
-print get_size("/home/matthew/Projects/CS537")
+getFolderSize("/home/matthew/Projects/CS537")
